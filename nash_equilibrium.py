b87ff84469a3ec2863b9e864b82bfdd994cd52c8
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-04-30 16:13:44 +0200

    nash_equilibrium added

---
+++ nash_equilibrium.py
@@ -0,0 +1,5 @@
+from koneroj import *
+nash_equilibrium = writable_concept("Nash equilibrium")
+nash_equilibrium.parts["Definition"].desplay += r""" A nash equilibrium is a stable state in a game where all players will conitue with their current statergy if they have full knoledge of all other players stratergies."""
+nash_equilibrium.write_module_doc()
+
