4582d88396949a055751693b2f2d8ea076770646
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-04-30 14:42:45 +0200

    residual_demand written

---
+++ residual_demand.py
@@ -0,0 +1,4 @@
+from koneroj import *
+residual_demand = writable_concept("Residual demand")
+residual_demand.parts["Definition"].desplay += r""" The residual demand is the demand curve facing an industry after acounting for the trades some of suppliers may make."""
+residual_demand.write_module_doc()  
